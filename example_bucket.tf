3a00496e9a8008766881b46470e9289ac654db97
glennbech <glenn.bech@gmail.com>
2020-10-11 21:23:42 +0200

    interpolation

--- example_bucket.tf
+++ example_bucket.tf
@@ -3,3 +3,5 @@ resource "google_storage_bucket" "static-site" {
   name          = "glennsdronefootage"
   location      = "EU"
 }
+
+
--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "google-beta" {
-  credentials = "${file("./terraform.json")}"
+  credentials = file("terraforn.json")
   project     = "terraform-292215"
   version = "~> 3.0.0-beta.1"
 }
