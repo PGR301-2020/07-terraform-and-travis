f229faba2869de01bfeb65aa79ebb2e5c0840f47
glennbech <glenn.bech@gmail.com>
2020-10-11 22:01:58 +0200

    Not public anymore

--- main.tf
+++ main.tf
@@ -17,18 +17,3 @@ resource "google_cloud_run_service" "hello" {
 }
 
 
-data "google_iam_policy" "noauth" {
-  binding {
-    role = "roles/run.invoker"
-    members = [
-      "allUsers",
-    ]
-  }
-}
-
-resource "google_cloud_run_service_iam_policy" "noauth" {
-  location    = google_cloud_run_service.hello.location
-  project     = google_cloud_run_service.hello.project
-  service     = google_cloud_run_service.hello.name
-  policy_data = data.google_iam_policy.noauth.policy_data
-}
