c4c0006d854afe413c897fd568ad605698b13bb1
glennbech <glenn.bech@gmail.com>
2020-10-25 18:25:23 +0100

    syntax

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "google_cloud_run_service" "hello" {
         image = "gcr.io/terraform-292215/helloworld@sha256:11b936b0a2f6adf879d7167291e46cca44dae155f70bfb210bb8731edfe8589e"
         env {
           name = "LOGZ_TOKEN"
-          value = "${var.logz_token}"
+          value = var.logz_token
         }
       }
     }
