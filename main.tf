103810f043861dc06599f17d52045c88ab10881c
glennbech <glenn.bech@gmail.com>
2020-10-26 10:48:12 +0100

    Ups det var en feil, vi ruller tilbake,

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "google_cloud_run_service" "hello" {
   template {
     spec {
       containers {
-        image = "gcr.io/terraform-292215/helloworld@sha256:11b936b0a2f6adf879d7167291e46cca44dae155f70bfb210bb8731edfe8589e"
+        image = "gcr.io/terraform-292215/helloworld@sha256:0ee92532317e87faadbe0231986e827605d1d97cbf9acd9c44557f49a416867d"
         env {
           name = "LOGZ_TOKEN"
           value = var.logz_token
