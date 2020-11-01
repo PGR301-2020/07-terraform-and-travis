cc427f294b82797c7c45591494af6200399d9c7d
glennbech <glenn.bech@gmail.com>
2020-11-01 20:51:26 +0100

    uptime checks by statuscake

--- uptime_checks.tf
+++ uptime_checks.tf
@@ -1,28 +1,11 @@
-resource "google_monitoring_uptime_check_config" "http" {
-  display_name = "http-uptime-check"
-  timeout      = "60s"
-  project = var.project_id
-
-  http_check {
-    path = "/"
-    request_method = "GET"
-  }
-
-  monitored_resource {
-    type = "uptime_url"
-    labels = {
-      project_id = "terraform"
-      host       = module.urlparser.host
-    }
-  }
-
-  content_matchers {
-    content = "example"
-  }
+provider "statuscake" {
+  username = "postglennbechno"
 }
 
-module "urlparser" {
-  source = "matti/urlparse/external"
-  url = google_cloud_run_service.hello.status[0].url
-
+resource "statuscake_test" "google" {
+  website_name = "google.com"
+  website_url  = google_cloud_run_service.hello.status[0].url
+  test_type    = "HTTP"
+  check_rate   = 300
+  contact_id   = 12345
 }
