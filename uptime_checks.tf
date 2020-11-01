e30f2f852118ccdd501283fd30162996c2bd73ea
glennbech <glenn.bech@gmail.com>
2020-11-01 20:57:51 +0100

    uptime

--- uptime_checks.tf
+++ uptime_checks.tf
@@ -3,9 +3,11 @@ provider "statuscake" {
 }
 
 resource "statuscake_test" "google" {
-  website_name = "google.com"
+  website_name = "My test"
   website_url  = google_cloud_run_service.hello.status[0].url
   test_type    = "HTTP"
   check_rate   = 300
   contact_id   = 12345
 }
+
+resource "" "" {}
