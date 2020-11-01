432fe0f5e160e1ccf7be707c02da432f15a99ee0
glennbech <glenn.bech@gmail.com>
2020-11-01 22:32:15 +0100

    remove test to see if it helps

--- uptime_checks.tf
+++ uptime_checks.tf
@@ -1,11 +1,3 @@
 provider "statuscake" {
   username = "postglennbechno"
 }
-
-resource "statuscake_test" "google" {
-  website_name = "My test"
-  website_url = "https://letshopethisdoesnotexistatall.com"
-  test_type = "HTTP"
-  check_rate = 300
-  contact_id = 5723482
-}
