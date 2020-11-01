c8c044a4c2aa9fe109a4617d49347c162d855e5b
glennbech <glenn.bech@gmail.com>
2020-11-01 21:41:58 +0100

    Contact groups!

--- uptime_checks.tf
+++ uptime_checks.tf
@@ -7,7 +7,5 @@ resource "statuscake_test" "google" {
   website_url  = google_cloud_run_service.hello.status[0].url
   test_type    = "HTTP"
   check_rate   = 300
-  contact_id   = 12345
+  contact_groups = ["Default"]
 }
-
-resource "" "" {}
