e432525924717f4bc3322dc10b716f7d94b92d9e
glennbech <glenn.bech@gmail.com>
2020-11-01 22:28:51 +0100

    Conteat ID

--- uptime_checks.tf
+++ uptime_checks.tf
@@ -4,8 +4,8 @@ provider "statuscake" {
 
 resource "statuscake_test" "google" {
   website_name = "My test"
-  website_url  = "https://letshopethisdoesnotexistatall.com"
-  test_type    = "HTTP"
-  check_rate   = 300
-  contact_group = ["pgr301-glennbech"]
+  website_url = "https://letshopethisdoesnotexistatall.com"
+  test_type = "HTTP"
+  check_rate = 300
+  contact_id = 5723482
 }
